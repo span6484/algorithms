b01615e8ff286ac68b5553ba30a0c3af7baf9d22
chanchann <www.769440972@qq.com>
2020-06-20 16:10:48 +0800

    tree

---
+++ 04_tree/117_Populating_Next_Right_Pointers_in_Each_Node_II_01/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+        if(root->left && root->right) 
+            root->left->next = root->right;
+
+        if(root->left && !root->right) 
+            root->left->next = getNext(root->next);
+        if(root->right) 
+            root->right->next = getNext(root->next);
+        connect(root->right);   
+        connect(root->left);
+
+        return root;
+        
+    }
+    Node* getNext(Node* root) {
+        if(!root) return root;
+        if(root->left) return root->left;
+        if(root->right) return root->right;
+        return getNext(root->next);
+    } 
+};
